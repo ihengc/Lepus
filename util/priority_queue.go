2e3bb5d84534db90c5d4dc2989b72a8fe3874641
ihengc <ihengc@163.com>
2022-06-28 22:55:54 +0800

    优先级队列的插入

--- util/priority_queue.go
+++ util/priority_queue.go
@@ -4,7 +4,7 @@ package util
 * @author: Ihc
 * @date: 2022/6/28 0028 09:36
 * @version: 1.0
-* @description:
+* @description: 优先级队列
 *********************************************************/
 import (
 	"errors"
@@ -19,8 +19,6 @@ type Comparator interface {
 	// Compare 比较e1与e2的大小
 	Compare(e1, e2 interface{}) int
 }
-
-type Option func(*PriorityQueue)
 
 type IPriorityQueue interface {
 	// Add 将指定元素插入到队列中
@@ -41,13 +39,21 @@ type IPriorityQueue interface {
 	Contains(e interface{}) bool
 }
 
+// PriorityQueue 优先级队列
 type PriorityQueue struct {
-	size       int
+	// size 存放优先级队列中的元素大小
+	size int
+	// comparator 元素比较器
 	comparator Comparator
-	queue      []interface{}
+	// queue 存放元素的数组
+	queue []interface{}
 }
 
-// grow 调整底层数据大小
+// grow 数组扩容
+// 底层的数组需要定义初始容量，扩容需要有对应
+// 的扩容机制。当数组容量小于64时，每次扩容2
+// 个容量；若数组容量大于等于64时，每次扩容原
+// 来容量的1.5倍
 func (p *PriorityQueue) grow() {
 	var (
 		oldCapacity int
@@ -65,13 +71,7 @@ func (p *PriorityQueue) grow() {
 	p.queue = newQueue
 }
 
-// Add 将元素e插入到队列中
-func (p *PriorityQueue) Add(e interface{}) (bool, error) {
-	return p.Offer(e)
-}
-
-// Offer 将元素e插入到队列中
-// 不允许插入为nil的元素
+// Offer 将指定元素插入到队列中
 func (p *PriorityQueue) Offer(e interface{}) (bool, error) {
 	if e == nil {
 		return false, NilPointErr
@@ -84,126 +84,29 @@ func (p *PriorityQueue) Offer(e interface{}) (bool, error) {
 	return true, nil
 }
 
-// siftUp 从指定位置处调整元素，使其满足堆的性质
+// siftUp 向上调整树的结构，使其满足堆的性质
+// 对于插入一个元素到堆中我们关心的是，新元素
+// 的加入能否让数组继续满足堆的性质。如何让数
+// 组继续满足堆的性质？因为树是一个递归定义的
+// 结构，所以我们只用关系被递归的单元是否满足
+// 堆的性质即可，也就是关心插入元素与其父节点
+// 值的大小。
+// (小根堆)
+// 若插入元素的值比其父节点的值要小，则不满足
+// 堆的性质，需要将其父节点的值移动到插入元素
+// 的位置。反之则说明已经满足了堆的性质。当整
+// 个树已经满足了堆的性质，也就是说明我们找到
+// 了新增元素正确地插入位置，我们记录插入的位
+// 置，然后在该位置放入新增元素即可。
 func (p *PriorityQueue) siftUp(k int, x interface{}) {
 	for k > 0 {
 		parentIndex := (k - 1) >> 1
 		parent := p.queue[parentIndex]
-		if p.comparator.Compare(parent, x) <= 0 {
+		if p.comparator.Compare(x, parent) >= 0 {
 			break
 		}
 		p.queue[k] = parent
 		k = parentIndex
 	}
 	p.queue[k] = x
-}
-
-// siftDown 保持位置k及以下的节点堆的特性
-func (p *PriorityQueue) siftDown(k int, x interface{}) {
-	n := p.size
-	half := n >> 1
-	for k < half {
-		childIndex := (k << 1) + 1
-		child := p.queue[childIndex]
-		rightIndex := childIndex + 1
-		if rightIndex < n && p.comparator.Compare(child, p.queue[rightIndex]) > 0 {
-			child = p.queue[childIndex]
-		}
-		if p.comparator.Compare(x, child) <= 0 {
-			break
-		}
-		p.queue[k] = child
-		k = childIndex
-	}
-	p.queue[k] = x
-}
-
-// indexOf 返回元素e在底层数组中的索引
-// 若元素e不在数组中，则返回-1
-func (p *PriorityQueue) indexOf(e interface{}) int {
-	if e != nil {
-		n := p.size
-		for i := 0; i < n; i++ {
-			if p.comparator.Compare(p.queue[i], e) == 0 {
-				return i
-			}
-		}
-	}
-	return -1
-}
-
-// removeAt 移除指定索引处的元素
-// 返回被移除的元素
-func (p *PriorityQueue) removeAt(i int) interface{} {
-	ns := p.size - 1
-	if ns == i {
-		p.queue[i] = nil
-	} else {
-
-	}
-	return nil
-}
-
-// Remove 从队列中移除指定元素的单个实例(若元素存在)
-func (p *PriorityQueue) Remove(e interface{}) bool {
-	i := p.indexOf(e)
-	if i == -1 {
-		return false
-	} else {
-		p.removeAt(i)
-		return true
-	}
-}
-
-// Contains 若此队列包含元素e，则返回true；否则返回false
-func (p *PriorityQueue) Contains(e interface{}) bool {
-	return p.indexOf(e) >= 0
-}
-
-// Peek 检索但不删除此队列的头部，如果此队列为空，则返回nil
-func (p *PriorityQueue) Peek() interface{} {
-	return p.queue[0]
-}
-
-// Poll 检索并删除此队列的头部，如果此队列为空，则返回nil
-func (p *PriorityQueue) Poll() interface{} {
-	return nil
-}
-
-// Clear 清空队列
-// 保持底层数组元素个数和容量不变
-// 不更换底层数组
-func (p *PriorityQueue) Clear() {
-	for i := 0; i < cap(p.queue); i++ {
-		p.queue[i] = nil
-	}
-}
-
-// Size 返回队列中的元素个数
-func (p *PriorityQueue) Size() int {
-	return p.size
-}
-
-// SetComparator 设置比较器
-func SetComparator(comparator Comparator) Option {
-	if comparator == nil {
-		panic(NilPointErr)
-	}
-	return func(queue *PriorityQueue) {
-		queue.comparator = comparator
-	}
-}
-
-// NewPriorityQueue 创建优先级队列
-func NewPriorityQueue(initialCapacity int, options ...Option) *PriorityQueue {
-	p := new(PriorityQueue)
-	for _, opt := range options {
-		opt(p)
-	}
-	if initialCapacity <= 0 {
-		p.queue = make([]interface{}, 11, 11)
-	} else {
-		p.queue = make([]interface{}, initialCapacity, initialCapacity)
-	}
-	return p
 }
