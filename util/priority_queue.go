6a848ff2f8a07d1168d609036543df037086dd6b
ihengc <ihengc@163.com>
2022-06-29 11:05:18 +0800

    implement priority queue

--- util/priority_queue.go
+++ util/priority_queue.go
@@ -71,6 +71,11 @@ func (p *PriorityQueue) grow() {
 	p.queue = newQueue
 }
 
+// Add 将指定元素插入到队列中
+func (p *PriorityQueue) Add(e interface{}) (bool, error) {
+	return p.Offer(e)
+}
+
 // Offer 将指定元素插入到队列中
 func (p *PriorityQueue) Offer(e interface{}) (bool, error) {
 	if e == nil {
@@ -109,4 +114,123 @@ func (p *PriorityQueue) siftUp(k int, x interface{}) {
 		k = parentIndex
 	}
 	p.queue[k] = x
+}
+
+// Remove 从队列中移除指定元素的单个实例(若元素存在)
+func (p *PriorityQueue) Remove(e interface{}) bool {
+	i := p.indexOf(e)
+	if i == -1 {
+		return false
+	} else {
+		p.removeAt(i)
+		return true
+	}
+}
+
+// indexOf 返回元素在底层数组中的索引
+// 复杂度O(n)
+func (p *PriorityQueue) indexOf(e interface{}) int {
+	if e != nil {
+		for i := 0; i < p.size; i++ {
+			if p.comparator.Compare(e, p.queue[i]) == 0 {
+				return i
+			}
+		}
+	}
+	return -1
+}
+
+// removeAt 删除指定位置处的元素，若指定处的元素存在，则
+// 返回被删除的元素；否则返回nil
+func (p *PriorityQueue) removeAt(i int) interface{} {
+	n := p.size - 1
+	p.size--
+	if i == n {
+		p.queue[i] = nil
+	} else {
+		moved := p.queue[n]
+		p.queue[n] = nil
+		p.siftDown(i, moved)
+		// 说明当前元素以下的树结构满足堆的性质
+		if p.comparator.Compare(p.queue[i], moved) == 0 {
+			p.siftUp(i, moved)
+			if p.comparator.Compare(p.queue[i], moved) != 0 {
+				return moved
+			}
+		}
+	}
+	return nil
+}
+
+// siftDown 向下调整树结构，使其满足堆的性质
+// 树的结构我们关注的是父节点(当前节点)与其子
+// 节点的大小关系
+func (p *PriorityQueue) siftDown(k int, x interface{}) {
+	half := p.size >> 1
+	for k < half {
+		childIndex := (k << 1) + 1
+		child := p.queue[childIndex]
+		rightIndex := childIndex + 1
+		if rightIndex < p.size && p.comparator.Compare(child, p.queue[rightIndex]) > 0 {
+			break
+		}
+		p.queue[k] = child
+		k = childIndex
+	}
+	p.queue[k] = x
+}
+
+// Poll 检索并删除此队列的头部，如果此队列为空，则返回nil
+func (p *PriorityQueue) Poll() interface{} {
+	if p.size == 0 {
+		return nil
+	}
+	n := p.size - 1
+	p.size--
+	result := p.queue[0]
+	moved := p.queue[n]
+	p.queue[n] = nil
+	if n != 0 {
+		p.siftDown(0, moved)
+	}
+	return result
+}
+
+// Size 返回队列中的元素个数
+func (p *PriorityQueue) Size() int {
+	return p.size
+}
+
+// Peek 检索但不删除此队列的头部，如果此队列为空，则返回nil
+func (p *PriorityQueue) Peek() interface{} {
+	return p.queue[0]
+}
+
+// Contains 如果此队列包含指定元素，则返回 true
+func (p *PriorityQueue) Contains(e interface{}) bool {
+	return p.indexOf(e) != -1
+}
+
+// Clear 清空队列
+func (p *PriorityQueue) Clear() {
+	n := p.size
+	for i := 0; i < n; i++ {
+		p.queue[i] = nil
+	}
+}
+
+// NewPriorityQueue 创建优先级队列
+// 这里完全信任了用户的初始容量数据
+func NewPriorityQueue(initialCapacity int, comparator Comparator) *PriorityQueue {
+	if comparator == nil {
+		panic(NilPointErr)
+	}
+	p := new(PriorityQueue)
+	if initialCapacity < 0 {
+		p.queue = make([]interface{}, 11, 11)
+	} else {
+		p.queue = make([]interface{}, initialCapacity, initialCapacity)
+	}
+	p.comparator = comparator
+	return p
 }
